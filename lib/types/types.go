66f9a86c826493ee1bdc58a07d2140555fc65e80
aeaton <aeaton@batswireless.com>
2026-03-20 08:53:58 -0400

    Update types.go

--- lib/types/types.go
+++ lib/types/types.go
@@ -1,19 +1,6 @@
 package types
 
-// Track mirrors the server's Track type.
-// type Track struct {
-// 	ID                 string  `json:"id"`
-// 	Filename           string  `json:"filename"`
-// 	Title              string  `json:"title"`
-// 	Artist             string  `json:"artist"`
-// 	Album              string  `json:"album"`
-// 	Year               int     `json:"year"`
-// 	FilePath           string  `json:"file_path"`
-// 	CoverArtID         string  `json:"cover_art_id"`
-// 	Duration           string  `json:"duration"`
-// 	DurationSec        float64 `json:"duration_seconds"`
-// 	PlaylistPositionID string  `json:"playlist_position_id"` // Unique ID per playlist position (empty for non-playlist contexts)
-// }
+import "time"
 
 type Track struct {
 	ID                 string `json:"id"`
@@ -120,4 +107,12 @@ type AlbumPageData struct {
 	PageSize     int
 	TotalPages   int
 	Search       string
+}
+
+type SessionInfo struct {
+	ID              string    `json:"id"`
+	Name            string    `json:"name"`
+	HasPlayer       bool      `json:"has_player"`
+	ControllerCount int       `json:"controller_count"`
+	LastSeen        time.Time `json:"last_seen"`
 }
